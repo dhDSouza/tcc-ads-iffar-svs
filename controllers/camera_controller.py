3a6875db18a54771ecb338705b75ca26eddae6e3
dhDSouza <dhs.danielsouza@gmail.com>
2023-11-20 04:31:43 -0300

    Adjust routes 'adcionar_camera', 'editar_camera' and 'video_feed'

--- controllers/camera_controller.py
+++ controllers/camera_controller.py
@@ -1,8 +1,7 @@
+import os
 from flask import Blueprint, render_template, url_for, request, jsonify, session, redirect, Response
 from models.camera_model import Camera
-from json import dumps
-from utils.deteccao_pessoas import detectar_pessoas
-import os
+from utils.deteccao_pessoas import realizar_contagem, realizar_heatmap
 
 camera_bp = Blueprint('camera', __name__, url_prefix='/camera')
 
@@ -27,20 +26,36 @@ def adicionar_camera():
     if verificar_login():
 
         if request.method == 'GET':
+            
             return render_template('add_camera.html')
 
         else:
 
             name = request.form.get('name')
             ip = request.form.get('ip')
+            contagem_checkbox = request.form.get('contagem')
 
             exist_camera = Camera().get_camera_by_ip(ip)
 
             if exist_camera:
-                data = 'Já existe câmera regitrada com este IP!'
+                data = 'Já existe câmera registrada com este IP!'
                 return render_template('add_camera.html', data=data)
 
-            new_camera = Camera().add_camera(name, ip)
+            if contagem_checkbox:
+
+                linha_form = request.form.get('linha')
+                linha = list(map(int, linha_form.split(',')))
+                entrada = request.form.get('entrada')
+
+                if not (linha[0] == linha[2] or linha[1] == linha[3]):
+                    data = 'A linha precisa ser vertical ou horizontal!'
+                    return render_template('add_camera.html', data=data)
+
+                new_camera = Camera().add_camera(name, ip, linha=linha, entrada=entrada)
+
+            else:
+
+                new_camera = Camera().add_camera(name, ip)
 
             return redirect(url_for('camera.listar_cameras'))
 
@@ -64,18 +79,31 @@ def editar_camera(id):
 
             name = request.form.get('name')
             ip = request.form.get('ip')
+            contagem_checkbox = request.form.get('contagem')
+
+            if contagem_checkbox:
+
+                linha_form = request.form.get('linha')
+                linha = list(map(int, linha_form.split(',')))
+                entrada = request.form.get('entrada')
+        
+            else:
+
+                linha = None
+                entrada = None
 
             exist_ip = Camera().get_camera_by_ip(ip)
 
             if exist_ip and camera.get('ip') != ip:
-                data = 'Já existe câmera regitrada com este IP!'
+                data = 'Já existe câmera registrada com este IP!'
                 return render_template('edit_camera.html', camera=camera, data=data)
 
-            new_camera = Camera().update_camera(id, name, ip)
+            new_camera = Camera().update_camera(id, name, ip, new_linha=linha, new_entrada=entrada)
 
             return redirect(url_for('camera.listar_cameras'))
 
     return redirect(url_for('auth.login'))
+
 
 @camera_bp.route('/excluir/<id>', methods=['GET'])
 def excluir_camera(id):
@@ -111,6 +139,18 @@ def visualizar_camera(camera_id):
 
 @camera_bp.route('/video_feed/<camera_id>')
 def video_feed(camera_id):
+    camera = Camera().get_camera_by_id(camera_id)
 
-    return Response(detectar_pessoas(Camera().get_camera_by_id(camera_id)['ip']),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    if camera:
+        linha = camera.get('linha')
+        entrada = camera.get('entrada')
+        camera_ip = camera.get('ip')
+
+        if linha and entrada:
+            return Response(realizar_contagem(camera_ip, linha, entrada),
+                        mimetype='multipart/x-mixed-replace; boundary=frame')
+        else:
+            return Response(realizar_heatmap(camera_ip),
+                        mimetype='multipart/x-mixed-replace; boundary=frame')
+    else:
+        return redirect(url_for('camera.listar_cameras'))
