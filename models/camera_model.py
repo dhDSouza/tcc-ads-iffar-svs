753a0b7bd62735208ef74cc1d5101f18a6b6438d
dhDSouza <dhs.danielsouza@gmail.com>
2023-11-20 04:25:29 -0300

    Include new fields in camera_model

--- models/camera_model.py
+++ models/camera_model.py
@@ -1,14 +1,21 @@
 from bson import ObjectId
 
 class Camera:
-    def __init__(self, name=None, ip=None):
+    def __init__(self, name=None, ip=None, linha=None, entrada=None):
         self.name = name
         self.ip = ip
+        self.linha = linha
+        self.entrada = entrada
 
-    def add_camera(self, name, ip):
+    def add_camera(self, name, ip, linha=None, entrada=None):
         from app import mongo
-        mongo.get_database().get_collection('cameras').insert_one({'name': name, 'ip': ip})
-
+        mongo.get_database().get_collection('cameras').insert_one({
+            'name': name,
+            'ip': ip,
+            'linha': linha,
+            'entrada': entrada
+        })
+    
     def list_cameras(self):
         from app import mongo
         return list(mongo.get_database().get_collection('cameras').find())
@@ -21,12 +28,11 @@ class Camera:
         from app import mongo
         return mongo.get_database().get_collection('cameras').find_one({'ip': ip})
 
-    def update_camera(self, camera_id, new_name, new_ip):
+    def update_camera(self, camera_id, new_name, new_ip, new_linha=None, new_entrada=None):
         from app import mongo
         mongo.get_database().get_collection('cameras').find_one_and_update(
             {'_id': ObjectId(camera_id)},
-            {'$set': {'name': new_name, 'ip': new_ip}},
-            return_document=True
+            {'$set': {'name': new_name, 'ip': new_ip, 'linha': new_linha, 'entrada': new_entrada}}
         )
 
     def delete_camera(self, camera_id):
